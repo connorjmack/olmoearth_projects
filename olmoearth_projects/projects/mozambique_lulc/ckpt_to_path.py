e6f26228137cbdce68d1ffe3336f295f0525e8be
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-11-17 13:32:27 +0000

    just save the encoder

--- olmoearth_projects/projects/mozambique_lulc/ckpt_to_path.py
+++ olmoearth_projects/projects/mozambique_lulc/ckpt_to_path.py
@@ -30,6 +30,11 @@ if __name__ == "__main__":
     # remove the ".model"
     new_sd: dict[str, Any] = {}
     for key, val in state_dict.items():
-        new_sd[key.replace("model.", "").replace("encoder.0", "encoder")] = val
+        if "encoder" in key:
+            new_sd[
+                key.replace("model.", "")
+                .replace("encoder.0", "encoder")
+                .replace("encoder.", "")
+            ] = val
 
     torch.save(new_sd, f"{args.save_path}/weights.pth")

f448d3a56ec9972f9a0f9a34edc7968d72a17290
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-12-05 08:14:36 +0000

    Add some verbosity

--- olmoearth_projects/projects/mozambique_lulc/check_label_quality.py
+++ olmoearth_projects/projects/mozambique_lulc/check_label_quality.py
@@ -46,4 +46,5 @@ if __name__ == "__main__":
             geometry.append(box(*window.bounds))
 
     df = gpd.GeoDataFrame({"label": labels, "geometry": geometry})
+    print(f"Checking label quality for {len(df)} instances.")
     check_label_quality(df)
